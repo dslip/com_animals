7dcb72e95f600edeae9f2d3210d3630acd1f21ce
speeD Demon <speeddemon@dslip.com.au>
2012-11-20 21:40:46 +1030

    base files

---
+++ site/views/animals/view.html.php
@@ -0,0 +1,22 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla view library
+jimport('joomla.application.component.view');
+ 
+/**
+ * HTML View class for the Animals Component
+ */
+class AnimalsViewAnimals extends JView
+{
+        // Overwriting JView display method
+        function display($tpl = null) 
+        {
+                // Assign data to the view
+                $this->msg = 'Search or Browse German Shepherd Dog Listings';
+ 
+                // Display the view
+                parent::display($tpl);
+        }
+}
